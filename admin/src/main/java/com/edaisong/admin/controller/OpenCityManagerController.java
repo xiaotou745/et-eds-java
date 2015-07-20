ee5fc2a2d6901a35701cb18a1a04865b86795f8f
caoheyang <cao.heyang@etaostars.com>
2015-07-20 11:01:28 +0800

    开通城市管理

---
+++ admin/src/main/java/com/edaisong/admin/controller/OpenCityManagerController.java
@@ -0,0 +1,24 @@
+package com.edaisong.admin.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("OpenCityManager")
+public class OpenCityManagerController {
+	@RequestMapping("OpenCityManager")
+	public ModelAndView index(HttpServletRequest request,
+			HttpServletResponse response) {
+		ModelAndView model = new ModelAndView("adminView");
+		model.addObject("subtitle", "管理员");
+		model.addObject("currenttitle", "开通城市管理");
+		model.addObject("viewPath", "opencityManager/openCityManager");
+		return model;
+	}
+}
