bc80ac3e1a648d8024bbbd73b02aa0c2c9c8b404
pengyi <peng.yi@etaostarts.com>
2015-07-20 09:53:53 +0800

    添加domain model文件夹,专门存放数据库多表查询model

---
+++ entity/src/main/java/com/edaisong/domain/testuser/TestUserDM.java
@@ -0,0 +1,32 @@
+package com.edaisong.domain.testuser;
+
+public class TestUserDM {
+    private int id;
+    private String phoneNo;
+    private boolean isC;
+    private boolean isB;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public boolean isC() {
+		return isC;
+	}
+	public void setC(boolean isC) {
+		this.isC = isC;
+	}
+	public boolean isB() {
+		return isB;
+	}
+	public void setB(boolean isB) {
+		this.isB = isB;
+	}
+}
