228d2f8e4f205a33afc987bb31937ea9e67ecdac
pengyi <peng.yi@etaostarts.com>
2015-07-20 14:08:28 +0800

    添加测试类

--- api/src/main/java/com/edaisong/api/dal/dao/impl/TestUserTblDao.java
+++ api/src/main/java/com/edaisong/api/dal/dao/impl/TestUserTblDao.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.edaisong.api.dal.dao.inter.ITestUserTblDao;
 import com.edaisong.entity.TestUserTbl;
 import com.edaisong.entity.domain.testuser.TestUserRecord;
 
+@Repository
 public class TestUserTblDao implements ITestUserTblDao {
 	@Autowired
 	private SqlSessionFactory superManReadOnlySqlServerSessionFactory;
