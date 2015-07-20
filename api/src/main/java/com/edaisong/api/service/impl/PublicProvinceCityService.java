292d7a68910c65d67fba0449d1f88e23fb52aef2
caoheyang <cao.heyang@etaostars.com>
2015-07-20 11:59:27 +0800

    城市

---
+++ api/src/main/java/com/edaisong/api/service/impl/PublicProvinceCityService.java
@@ -0,0 +1,20 @@
+package com.edaisong.api.service.impl;
+
+import java.util.List;
+
+import com.edaisong.api.service.inter.IPublicProvinceCityService;
+import com.edaisong.entity.PublicProvinceCity;
+
+
+public class PublicProvinceCityService implements IPublicProvinceCityService {
+
+
+	/**
+	 * 获取开放城市列表（非分页）
+	 * @author CaoHeYang 
+	 */
+	@Override
+	public  List<PublicProvinceCity> getOpenCityList(String  cityName) {
+		return null;
+	};
+}
