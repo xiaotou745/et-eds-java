c3eab6a633a1059951f4aa917c14550b866b8417
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-07-21 15:54:33 +0800

    公共配置数据获取完成

--- admin/src/main/java/com/edaisong/admin/controller/AdminToolsController.java
+++ admin/src/main/java/com/edaisong/admin/controller/AdminToolsController.java
@@ -1,6 +1,9 @@
 package com.edaisong.admin.controller;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javassist.compiler.ast.Variable;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.edaisong.api.service.inter.IAdminToolsService;
 import com.edaisong.api.service.inter.ITestService;
+import com.edaisong.entity.domain.GlobalConfigModel;
 import com.edaisong.entity.req.TestServiceReq;
 import com.edaisong.entity.resp.TestServiceResp;
 /*
@@ -21,14 +26,17 @@ import com.edaisong.entity.resp.TestServiceResp;
 @Controller
 @RequestMapping("admintools")
 public class AdminToolsController {
+	@Autowired
+	private IAdminToolsService adminToolsService;
 	@RequestMapping("globalconfigmanager")
 	public ModelAndView GlobalConfigManager(HttpServletRequest request, HttpServletResponse res)
 	{
+		List<GlobalConfigModel> data= adminToolsService.getGlobalConfigByGroupId(0);
 		ModelAndView model = new ModelAndView("adminView");
 		model.addObject("subtitle", "管理员");
 		model.addObject("currenttitle", "公共变量管理");
 		model.addObject("viewPath", "admintools/globalconfigmanager");
-		model.addObject("AAA","BBB");
+		model.addObject("DataList",data);
 		return model;
 	}
 
