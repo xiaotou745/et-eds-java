890147f58441cfb7439bfcf65163545b01de446c
pengyi <peng.yi@etaostarts.com>
2015-07-21 10:04:02 +0800

    找回丢失的代码

---
+++ entity/src/main/java/com/edaisong/entity/common/StringRequest.java
@@ -0,0 +1,18 @@
+package com.edaisong.entity.common;
+
+/**
+ * 通用string参数请求
+ * @author pengyi
+ * @Create Date:2015-07-21
+ */
+public class StringRequest extends RequestBase{
+	private String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+}
