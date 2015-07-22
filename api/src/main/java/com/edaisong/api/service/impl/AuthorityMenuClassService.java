47ee0d1dd07f27d98d73d56520f5e8129f42fb6f
zhaohailong <zhao.hailong@etaostars.com>
2015-07-22 17:15:42 +0800

    从git中移除.metadata

--- api/src/main/java/com/edaisong/api/service/impl/AuthorityMenuClassService.java
+++ api/src/main/java/com/edaisong/api/service/impl/AuthorityMenuClassService.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edaisong.api.dal.dao.inter.IAuthorityMenuClassDao;
-
 import com.edaisong.api.service.inter.IAuthorityMenuClassService;
-
 import com.edaisong.entity.MenuEntity;
+import com.edaisong.entity.common.ResponseCode;
 import com.edaisong.entity.req.AuthorityMenuReq;
 import com.edaisong.entity.resp.AuthorityMenuResp;
 
@@ -17,9 +16,18 @@ import com.edaisong.entity.resp.AuthorityMenuResp;
 public class AuthorityMenuClassService implements IAuthorityMenuClassService {
 	@Autowired
 	private IAuthorityMenuClassDao dao;
+	
 	@Override
 	public AuthorityMenuResp getMenuListByUserID(AuthorityMenuReq req) {
 		AuthorityMenuResp resp = new AuthorityMenuResp();
+		if(req==null||
+			req.getAccountId()==null||
+			req.getAccountId().isEmpty()){
+			resp.setResponseCode(ResponseCode.PARAMETER_NULL_ERROR);
+			resp.setMessage("入参或AccountId不能为空");
+			return resp;
+		}
+		
 		List<MenuEntity> listData = dao.getMenuListByUserID(
 				req.getAccountId());
 		resp.setMenuList(listData);
