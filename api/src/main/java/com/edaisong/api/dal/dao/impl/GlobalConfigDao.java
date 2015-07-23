a9d376aaba5c9be75c2e8dbf3f25b1ea30a95b51
hulingbo <hu.lingbo.etaostars.com>
2015-07-23 18:01:41 +0800

    jsp

--- api/src/main/java/com/edaisong/api/dal/dao/impl/GlobalConfigDao.java
+++ api/src/main/java/com/edaisong/api/dal/dao/impl/GlobalConfigDao.java
@@ -97,7 +97,7 @@ public class GlobalConfigDao extends DaoBase implements IGlobalConfigDao {
 		paramMap.put("keyname", par.getKeyname());
 		paramMap.put("value", par.getValue());
 		paramMap.put("remark", par.getRemark());
-		return SqlSessionUtil.wapperSession(superManSqlServerSessionFactory)
+		return getMasterSqlSessionUtil()
 				.insert(statement, paramMap) > 0;
 	}
 
--- api/src/main/java/com/edaisong/api/dal/dao/impl/GroupDao.java
+++ api/src/main/java/com/edaisong/api/dal/dao/impl/GroupDao.java
@@ -21,7 +21,7 @@ public class GroupDao extends DaoBase implements IGroupDao {
 		return 0;
 	}
 
-	// @Override
+	 @Override
 	public int insert(Group record) {
 		Map<String, Object> paramMap = new HashMap<>();
 		paramMap.put("groupname", record.getGroupname());
@@ -73,7 +73,7 @@ public class GroupDao extends DaoBase implements IGroupDao {
 	public List<GroupModel> getGroupListByID(Long id) {
 		Map<String, Object> paramMap = new HashMap<>();
 		paramMap.put("id", id);
-		List<GroupModel> list = getReadOnlySqlSessionUtil().selectList(
+		List<GroupModel> list = getMasterSqlSessionUtil().selectList(
 				"com.edaisong.api.dal.dao.inter.IGroupDao.getGroupListByID",
 				paramMap);
 		return list;
@@ -86,7 +86,7 @@ public class GroupDao extends DaoBase implements IGroupDao {
 		// paramMap.put("id", id);
 		paramMap.put("groupname", req.getGroupName());
 		paramMap.put("appkey", req.getAppKey());
-		List<GroupModel> list = getReadOnlySqlSessionUtil().selectList(
+		List<GroupModel> list = getMasterSqlSessionUtil().selectList(
 				"com.edaisong.api.dal.dao.inter.IGroupDao.getGroupList",
 				paramMap);
 		return list;
