3c2a200c77976ea5c2abd0a7b6c19733d43b7657
caoheyang <cao.heyang@etaostars.com>
2015-07-27 16:11:32 +0800

    订单

---
+++ api/src/main/java/com/edaisong/api/dal/dao/impl/OrderDao.java
@@ -0,0 +1,18 @@
+package com.edaisong.api.dal.dao.impl;
+
+import com.edaisong.api.dal.dao.inter.IOrderDao;
+import com.edaisong.core.util.SqlSessionUtil;
+import com.edaisong.entity.Order;
+import com.edaisong.entity.domain.OrderListModel;
+
+public class OrderDao  extends DaoBase implements IOrderDao {
+
+	public void ss(){
+		OrderListModel  pi=new OrderListModel();
+		
+	
+		
+	}
+
+
+}
