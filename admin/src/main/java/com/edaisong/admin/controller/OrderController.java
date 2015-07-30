738ced6308f9077671a3b1c586fb413a43d8fca3
caoheyang <cao.heyang@etaostars.com>
2015-07-30 16:31:58 +0800

    HtmlHelper.java

--- admin/src/main/java/com/edaisong/admin/controller/OrderController.java
+++ admin/src/main/java/com/edaisong/admin/controller/OrderController.java
@@ -1,8 +1,6 @@
 package com.edaisong.admin.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import javassist.expr.NewArray;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
@@ -10,17 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import com.edaisong.api.service.impl.OrderService;
 import com.edaisong.api.service.inter.IOrderService;
+import com.edaisong.api.service.inter.IPublicProvinceCityService;
 import com.edaisong.api.service.inter.ITestService;
-import com.edaisong.entity.Account;
+import com.edaisong.core.common.HtmlHelper;
 import com.edaisong.entity.common.PagedResponse;
-import com.edaisong.entity.domain.OpenCityModel;
+import com.edaisong.entity.domain.AreaModel;
 import com.edaisong.entity.domain.OrderListModel;
-import com.edaisong.entity.req.AccountReq;
 import com.edaisong.entity.req.PagedOrderSearchReq;
-import com.edaisong.entity.req.TestServiceReq;
-import com.edaisong.entity.resp.TestServiceResp;
 
 @Controller
 @RequestMapping("order")
@@ -34,17 +29,23 @@ public class OrderController {
 	 @Autowired
 	 private IOrderService orderService;
 	 
+	 @Autowired
+	 private IPublicProvinceCityService  iPublicProvinceCityService;
 	/**
 	 * 订单列表页面 
 	 * @author CaoHeYang
 	 * @Date 20150728
 	 * @return
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
 	 */
 	@RequestMapping("list")
-	public ModelAndView order(){
+	public ModelAndView order() throws IllegalArgumentException, IllegalAccessException{
+		List<AreaModel> areaListData=iPublicProvinceCityService.getOpenCityListFromRedis();
 		ModelAndView model = new ModelAndView("adminView");
 		model.addObject("subtitle", "订单管理");
 		model.addObject("currenttitle", "订单管理");
+		model.addObject("areaListData", areaListData);
 		model.addObject("viewPath", "order/list");
 		return model;
 	}
