af4c85e1be6bcc15237d74d4af9768217c14e2db
caoheyang <cao.heyang@etaostars.com>
2015-08-04 14:01:11 +0800

    xml

--- api/src/test/java/com/edaisong/api/AppTest.java
+++ api/src/test/java/com/edaisong/api/AppTest.java
@@ -7,6 +7,7 @@ import junit.framework.TestSuite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.edaisong.api.business.SqlSessionFactoryPool;
 import com.edaisong.api.service.inter.IOrderService;
 import com.edaisong.core.util.JsonUtil;
 import com.edaisong.entity.req.OrderDetailBusinessReq;
@@ -16,45 +17,45 @@ import com.edaisong.entity.resp.OrderDetailBusinessResp;
 /**
  * Unit test for simple App.
  */
-@Component
+
 public class AppTest 
     extends TestCase
 {
  
-	@Autowired
+
 	IOrderService testService;
 	
-//	public AppTest(){
-//		testService =  SqlSessionFactoryPool
-//				.getCustomBeanByType(IOrderService.class);
-//	}
+	public AppTest(){
+		testService =  SqlSessionFactoryPool
+				.getCustomBeanByType(IOrderService.class);
+	}
 	
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//    
     
     /**
      * Rigourous Test :-)
