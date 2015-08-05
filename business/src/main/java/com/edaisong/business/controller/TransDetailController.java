50e5cd6e6ea640637cde52658abdd08353750d0c
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-08-05 14:33:04 +0800

    添加交易明细页面

--- business/src/main/java/com/edaisong/business/controller/TransDetailController.java
+++ business/src/main/java/com/edaisong/business/controller/TransDetailController.java
@@ -1,7 +1,5 @@
 package com.edaisong.business.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +9,9 @@ import com.edaisong.api.service.inter.IBusinessFinanceService;
 import com.edaisong.entity.BusinessBalanceRecord;
 import com.edaisong.entity.common.PagedResponse;
 import com.edaisong.entity.req.TransDetailReq;
+
 /**
- * 商户中心控制器
- * 2015年8月4日15:20:52
- * 茹化肖
+ * 商户中心控制器 2015年8月4日15:20:52 茹化肖
  * 
  * 交易明细
  * 
@@ -24,23 +21,22 @@ import com.edaisong.entity.req.TransDetailReq;
 public class TransDetailController {
 	@Autowired
 	private IBusinessFinanceService businessFinanceService;
+
 	/**
-	 * 交易明细页面
-	 * 2015年8月4日15:20:28
-	 * 茹化肖
+	 * 交易明细页面 2015年8月4日15:20:28 茹化肖
 	 * */
 	@RequestMapping("list")
 	public ModelAndView transdetail() {
-		ModelAndView view = new ModelAndView();
-		TransDetailReq par=new TransDetailReq();
-		PagedResponse<BusinessBalanceRecord> result=businessFinanceService.getTransDetailList(par);
-		view.addObject("Data", result);
+		ModelAndView view = new ModelAndView("transdetail/list");
+		// TransDetailReq par=new TransDetailReq();
+		// PagedResponse<BusinessBalanceRecord>
+		// result=businessFinanceService.getTransDetailList(par);
+		// view.addObject("Data", result);
 		return view;
 	}
+
 	/**
-	 * 交易明细列表
-	 * 2015年8月4日15:22:26
-	 * 茹化肖
+	 * 交易明细列表 2015年8月4日15:22:26 茹化肖
 	 * */
 	@RequestMapping("listdo")
 	public ModelAndView listdo(TransDetailReq par) {
