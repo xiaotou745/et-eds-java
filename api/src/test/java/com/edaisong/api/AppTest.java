f83b2b7a828a85f7fce65dd98cff3e2b7d301323
caoheyang <cao.heyang@etaostars.com>
2015-08-05 16:06:25 +0800

    枚举

--- api/src/test/java/com/edaisong/api/AppTest.java
+++ api/src/test/java/com/edaisong/api/AppTest.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Field;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -20,102 +21,69 @@ import com.edaisong.entity.req.OrderDetailBusinessReq;
 import com.edaisong.entity.resp.CancelOrderBusinessResp;
 import com.edaisong.entity.resp.OrderDetailBusinessResp;
 
-
 /**
  * Unit test for simple App.
  */
 
-public class AppTest 
-    extends TestCase
-{
- 
+public class AppTest extends TestCase {
 
 	IOrderService testService;
-	
-	public AppTest(){
-		testService =  SqlSessionFactoryPool
+
+	public AppTest() {
+		testService = SqlSessionFactoryPool
 				.getCustomBeanByType(IOrderService.class);
 	}
-	
-//    /**
-//     * Create the test case
-//     *
-//     * @param testName name of the test case
-//     */
-//    public AppTest( String testName )
-//    {
-//        super( testName );
-//    }
-//
-//    /**
-//     * @return the suite of tests being tested
-//     */
-//    public static Test suite()
-//    {
-//        return new TestSuite( AppTest.class );
-//    }
-//
-//    /**
-//     * Rigourous Test :-)
-//     */
-//    public void testApp()
-//    {
-//        assertTrue( true );
-//    }
-//    
-    
-    /**
-     * Rigourous Test :-)
-     */
 
-    public void testOrder()
-    {
-    	OrderDetailBusinessReq para=new OrderDetailBusinessReq();
-    	para.setBusinessId(2008);
-    	para.setOrderNo("2008150630165417002");
-        OrderDetailBusinessResp res= testService.getOrderDetailBusiness(para);
-    	System.out.println(JsonUtil.obj2string(res));
-        assertTrue( true );
-      
-    }
+	// /**
+	// * Create the test case
+	// *
+	// * @param testName name of the test case
+	// */
+	// public AppTest( String testName )
+	// {
+	// super( testName );
+	// }
+	//
+	// /**
+	// * @return the suite of tests being tested
+	// */
+	// public static Test suite()
+	// {
+	// return new TestSuite( AppTest.class );
+	// }
+	//
+	// /**
+	// * Rigourous Test :-)
+	// */
+	// public void testApp()
+	// {
+	// assertTrue( true );
+	// }
+	//
 
-    public void testCanelOrder()
-    {
-    	CancelOrderBusinessReq cancelOrderBusinessReq=new CancelOrderBusinessReq();
-    	cancelOrderBusinessReq.setBusinessId(2008);
-    	cancelOrderBusinessReq.setOrderId(143525);
-    	cancelOrderBusinessReq.setOrderNo("2008150630165417002");
-    	
-        CancelOrderBusinessResp res= testService.cancelOrderBusiness(cancelOrderBusinessReq);	
-    }
-   
-    
-    public void testenum()
-    {
-    	
+	/**
+	 * Rigourous Test :-)
+	 */
 
-    	
+	public void testOrder() {
+		OrderDetailBusinessReq para = new OrderDetailBusinessReq();
+		para.setBusinessId(2008);
+		para.setOrderNo("2008150630165417002");
+		OrderDetailBusinessResp res = testService.getOrderDetailBusiness(para);
+		System.out.println(JsonUtil.obj2string(res));
+		assertTrue(true);
 
-    	
-    }
-    
-    @Documented
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface Person{
-        String name();
-    }
-    
-	@Person(name="sdasddasda")
-    public enum Cs{
-    	@Person(name="sssssssss")
-    	ss,
-    	@Person(name="xxxxxx")
-    	xxx,
-    }
-	
-	@Person(name="Css")
-	public   class  Css{
-		
-		
 	}
+
+	public void testCanelOrder() {
+		CancelOrderBusinessReq cancelOrderBusinessReq = new CancelOrderBusinessReq();
+		cancelOrderBusinessReq.setBusinessId(2008);
+		cancelOrderBusinessReq.setOrderId(143525);
+		cancelOrderBusinessReq.setOrderNo("2008150630165417002");
+
+		CancelOrderBusinessResp res = testService
+				.cancelOrderBusiness(cancelOrderBusinessReq);
+	}
+
+
 }
