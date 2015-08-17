d1d54896eb8c9078db9ff0f2b984e3bcf2c3ef7e
pengyi <peng.yi@etaostarts.com>
2015-08-17 13:24:50 +0800

    修改基本佣金+补贴

--- api/src/main/java/com/edaisong/api/service/impl/BaseCommissionOrPriceService.java
+++ api/src/main/java/com/edaisong/api/service/impl/BaseCommissionOrPriceService.java
@@ -1,9 +1,13 @@
 package com.edaisong.api.service.impl;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.edaisong.api.dal.dao.impl.GlobalConfigDao;
+import com.edaisong.entity.domain.GlobalGroupConfigModel;
 import com.edaisong.entity.domain.OrderCommission;
 
 /**
@@ -14,15 +18,30 @@ import com.edaisong.entity.domain.OrderCommission;
  */
 @Service
 public class BaseCommissionOrPriceService extends OrderPriceService{
-
+	
+	@Autowired
+	GlobalConfigDao globalConfigDao;
 	@Override
 	public BigDecimal getCurrenOrderCommission(OrderCommission model) {
-		return BigDecimal.valueOf(0);
+        if (model.getAmount() == null)
+        	return BigDecimal.valueOf(0);
+        BigDecimal distribe = getOrderWebSubsidy(model);  //默认外送费，网站补贴都为0
+        return getBaseCommission(model).add(distribe)
+        		.multiply(new BigDecimal(model.getOrderCount()))
+        		.setScale(2, RoundingMode.HALF_UP);//计算佣金:（基本佣金（可配置）+ 代收客配或网站补贴）*订单数量
 	}
 
 	@Override
 	public BigDecimal getOrderWebSubsidy(OrderCommission model) {
-		return BigDecimal.valueOf(0);
+        if (model.getOrderWebSubsidy() != null && !model.getOrderWebSubsidy().equals(BigDecimal.valueOf(0))){
+            return model.getOrderWebSubsidy();
+        }
+        GlobalGroupConfigModel globalConfigModel = globalConfigDao.GlobalConfigMethod(model.getBusinessGroupId());
+        if(globalConfigModel != null){
+        	return new BigDecimal(globalConfigModel.getBaseSiteSubsidies());
+        }else{
+        	return BigDecimal.valueOf(0);
+        }
 	}
 
 	@Override
@@ -32,7 +51,12 @@ public class BaseCommissionOrPriceService extends OrderPriceService{
 
 	@Override
 	public BigDecimal getBaseCommission(OrderCommission model) {
-		return BigDecimal.valueOf(0);
+        GlobalGroupConfigModel globalConfigModel = globalConfigDao.GlobalConfigMethod(model.getBusinessGroupId());
+        if(globalConfigModel != null){
+        	return new BigDecimal(globalConfigModel.getBaseCommission());
+        }else{
+        	return BigDecimal.valueOf(0);
+        }
 	}
 
 	@Override
