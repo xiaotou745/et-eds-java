457e18442a4fad7c132c753d29a9c87cd2cbb99b
zhaohailong <zhao.hailong@etaostars.com>
2015-09-09 11:15:19 +0800

    调整api-http项目的配置

--- api-http/src/main/java/com/edaisong/api_http/service/inter/ITestService.java
+++ api-http/src/main/java/com/edaisong/api_http/service/inter/ITestService.java
@@ -1,12 +1,16 @@
 package com.edaisong.api_http.service.inter;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.edaisong.api_http.entity.TestServiceReq;
 import com.edaisong.api_http.entity.TestServiceResp;
 
 @Path("/urlService")
+@Consumes("application/json")//当前方法接收的参数类型
+@Produces("application/json")//当前类的所有方法都返回json格式的数据
 public interface ITestService {
 	
 	@POST
