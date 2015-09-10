e056feb45c7f20dbe5d46223c0746800515fc407
zhaohailong <zhao.hailong@etaostars.com>
2015-09-10 17:15:14 +0800

    商家中心集团登录逻辑

--- api/src/main/java/com/edaisong/api/dao/inter/IGroupBusinessDao.java
+++ api/src/main/java/com/edaisong/api/dao/inter/IGroupBusinessDao.java
@@ -2,7 +2,8 @@ package com.edaisong.api.dao.inter;
 
 import com.edaisong.entity.Business;
 import com.edaisong.entity.GroupBusiness;
-import com.edaisong.entity.common.PagedResponse;import com.edaisong.entity.req.PagedGroupBusinessReq;
+import com.edaisong.entity.common.PagedResponse;
+import com.edaisong.entity.req.PagedGroupBusinessReq;
 public interface IGroupBusinessDao { 
     int insert(GroupBusiness record);
  
