b60317be798e9f8054f97af05321212a4478a2d0
caoheyang <cao.heyang@etaostars.com>
2015-09-10 09:50:38 +0800

    订单

---
+++ api-http/src/main/java/com/edaisong/api_http/service/impl/OrderHttpService.java
@@ -0,0 +1,26 @@
+package com.edaisong.api_http.service.impl;
+
+import com.edaisong.api_http.entity.ResultModel;
+import com.edaisong.api_http.service.inter.IOrderHttpService;
+
+
+/**
+ * 订单模块 
+ * @author CaoHeYang
+ * @date 20150910
+ */
+public class OrderHttpService implements IOrderHttpService {
+
+	/**
+	 * B端任务统计接口
+	 * @author CaoHeYang
+	 * @date 20150910
+	 * @param data 
+	 * @return
+	 */
+	@Override
+	public ResultModel<Object> orderStatisticsB(String data) {
+		return null;
+	}
+
+}
---
+++ api-http/src/main/java/com/edaisong/api_http/service/inter/IOrderHttpService.java
@@ -0,0 +1,30 @@
+package com.edaisong.api_http.service.inter;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.edaisong.api_http.entity.ResultModel;
+import com.edaisong.entity.resp.MessageResp;
+
+/**
+ * 订单模块 
+ * @author CaoHeYang
+ * @date 20150910
+ */
+@Path("/order")
+@Consumes("application/json")//当前方法接收的参数类型
+@Produces("application/json")//当前类的所有方法都返回json格式的数据
+public interface IOrderHttpService {
+	/**
+	 * B端任务统计接口
+	 * @author CaoHeYang
+	 * @date 20150910
+	 * @param data 
+	 * @return
+	 */
+	@POST
+	@Path("/orderstatisticsb")
+	 public ResultModel<Object> orderStatisticsB(String data);
+}
