49ec4da763c7756aea4367d1269618c8bac777f0
WangChao <wang.chao@etaostars.com>
2015-09-10 15:33:30 +0800

    集团管理

--- api/src/main/java/com/edaisong/api/dao/inter/IGroupBusinessDao.java
+++ api/src/main/java/com/edaisong/api/dao/inter/IGroupBusinessDao.java
@@ -1,6 +1,9 @@
 package com.edaisong.api.dao.inter;
 
 import com.edaisong.entity.GroupBusiness;
+import com.edaisong.entity.common.PagedResponse;
+import com.edaisong.entity.domain.GroupBusinessModel;
+import com.edaisong.entity.req.PagedGroupBusinessReq;
 
 public interface IGroupBusinessDao { 
     int insert(GroupBusiness record);
@@ -8,4 +11,7 @@ public interface IGroupBusinessDao {
     GroupBusiness selectByPrimaryKey(Integer id); 
     
     int updateByPrimaryKey(GroupBusiness record);
+    
+    PagedResponse<GroupBusinessModel> getPageList(
+			PagedGroupBusinessReq req);
 }
--- api/src/main/java/com/edaisong/api/service/impl/GroupBusinessService.java
+++ api/src/main/java/com/edaisong/api/service/impl/GroupBusinessService.java
@@ -1,20 +1,28 @@
 package com.edaisong.api.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.edaisong.api.dao.inter.IGroupBusinessDao;
+import com.edaisong.api.dao.inter.IGroupDao;
 import com.edaisong.api.service.inter.IGroupBusinessService;
-import com.edaisong.entity.GroupBusiness;
 import com.edaisong.entity.common.PagedResponse;
+import com.edaisong.entity.domain.GroupBusinessModel;
 import com.edaisong.entity.req.PagedGroupBusinessReq;
  
 
 @Service
 public class GroupBusinessService implements IGroupBusinessService {
 
+	@Autowired
+	private IGroupBusinessDao dao;
+	
 	@Override
-	public PagedResponse<GroupBusiness> getBusinessList(
+	public PagedResponse<GroupBusinessModel> getPageList(
 			PagedGroupBusinessReq req) {
 		// TODO Auto-generated method stub
-		return null;
-	} 
+		return dao.getPageList(req);
+	}
+
+	 
 }
