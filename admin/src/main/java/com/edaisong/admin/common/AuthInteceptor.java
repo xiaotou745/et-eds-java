369df5d4cfb2d239597abef5f1235cefe5b75702
pengyi <peng.yi@etaostarts.com>
2015-09-10 11:20:11 +0800

    删除测试代码

--- admin/src/main/java/com/edaisong/admin/common/AuthInteceptor.java
+++ admin/src/main/java/com/edaisong/admin/common/AuthInteceptor.java
@@ -58,7 +58,7 @@ public class AuthInteceptor extends HandlerInterceptorAdapter {
 			!request.getServletPath().equals("/account/code")&&
 			!request.getServletPath().equals("/order/list")&&
 			!request.getServletPath().equals("/order/exportorder")) {
-			return false;
+			return true;
 		}
 		return false;
 	}
