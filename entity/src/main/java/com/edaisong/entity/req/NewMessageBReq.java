7c5ff7946c713f0b703c04544e2a46a2aff90e4e
caoheyang <cao.heyang@etaostars.com>
2015-09-11 12:03:34 +0800

    度消息

---
+++ entity/src/main/java/com/edaisong/entity/req/NewMessageBReq.java
@@ -0,0 +1,18 @@
+package com.edaisong.entity.req;
+
+/**
+ * B端商户登陆后获取顶端未读公告
+ * @author CaoHeYang
+ *@date 20150911
+ */
+public class NewMessageBReq {
+private int businessId;
+public int getBusinessId() {
+	return businessId;
+}
+
+public void setBusinessId(int businessId) {
+	this.businessId = businessId;
+}
+
+}
