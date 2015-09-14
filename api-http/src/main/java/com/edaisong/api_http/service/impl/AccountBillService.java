b9e17a19f7c0aebeac583526d6266e3bf6f629a2
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-09-14 13:42:28 +0800

    C端账单查询完成

--- api-http/src/main/java/com/edaisong/api_http/service/impl/AccountBillService.java
+++ api-http/src/main/java/com/edaisong/api_http/service/impl/AccountBillService.java
@@ -1,24 +1,23 @@
 package com.edaisong.api_http.service.impl;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jdt.internal.compiler.ast.ThisReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edaisong.api.service.inter.IBusinessBalanceRecordService;
+import com.edaisong.api.service.inter.IClienterBalanceRecordService;
 import com.edaisong.api_http.service.inter.IAccountBillService;
 import com.edaisong.core.enums.returnenums.AccountBillReturnEnum;
-import com.edaisong.core.util.ParseHelper;
 import com.edaisong.entity.AccountBillResultModel;
 import com.edaisong.entity.common.HttpResultModel;
+import com.edaisong.entity.domain.AccountBillDayCResultModel;
 import com.edaisong.entity.domain.AccountBillDayResultModel;
+import com.edaisong.entity.domain.AccountBillDetailCModel;
 import com.edaisong.entity.domain.AccountBillDetailModel;
-import com.edaisong.entity.domain.AccountBillModel;
 import com.edaisong.entity.req.AccountBillBReq;
+import com.edaisong.entity.req.AccountBillCReq;
+import com.edaisong.entity.req.AccountBillDetailCReq;
 import com.edaisong.entity.req.AccountBillDetailReq;
+import com.edaisong.entity.req.PagedAccountBillDayCReq;
 import com.edaisong.entity.req.PagedAccountBillDayReq;
 
 /*
@@ -30,6 +29,8 @@ import com.edaisong.entity.req.PagedAccountBillDayReq;
 public class AccountBillService implements IAccountBillService {
 	@Autowired
 	private IBusinessBalanceRecordService businessBalanceRecordService;
+	@Autowired
+	private IClienterBalanceRecordService clienterBalanceRecordService;
 
 	/**
 	 * 获取商户月账单
@@ -59,6 +60,42 @@ public class AccountBillService implements IAccountBillService {
 	public HttpResultModel<AccountBillDetailModel> getBillDetail(AccountBillDetailReq par) {
 		AccountBillDetailModel model = businessBalanceRecordService.getAccountBillDetailB(par);
 		return new HttpResultModel<AccountBillDetailModel>().setMessage(AccountBillReturnEnum.Success.desc()).setStatus(AccountBillReturnEnum.Success.value())
+				.setResult(model);
+	}
+	/**
+	 * 骑士获取月账单 茹化肖 2015年9月14日09:48:29
+	 */
+	@Override
+	public HttpResultModel<AccountBillResultModel> getBillListC(
+			AccountBillCReq par) {
+		AccountBillResultModel listResult = clienterBalanceRecordService.getAccountBillListC(par);
+		return new HttpResultModel<AccountBillResultModel>().setMessage(AccountBillReturnEnum.Success.desc()).setResult(listResult)
+				.setStatus(AccountBillReturnEnum.Success.value());
+	}
+	/**
+	 * 
+	 * 骑士获取日账单
+	 * 茹化肖
+	 * 2015年9月14日11:08:44
+	 * 
+	 */
+	@Override
+	public HttpResultModel<AccountBillDayCResultModel> getBillListDayC(
+			PagedAccountBillDayCReq par) {
+		AccountBillDayCResultModel model = clienterBalanceRecordService.getAccountBillListDayC(par);
+		return new HttpResultModel<AccountBillDayCResultModel>().setMessage(AccountBillReturnEnum.Success.desc())
+				.setStatus(AccountBillReturnEnum.Success.value()).setResult(model);
+	}
+	/**
+	 * 骑士获取账单详情
+	 * 茹化肖
+	 * 2015年9月14日12:51:43
+	 */
+	@Override
+	public HttpResultModel<AccountBillDetailCModel> getBillDetailC(
+			AccountBillDetailCReq par) {
+		AccountBillDetailCModel model =clienterBalanceRecordService.getAccountBillDetailC(par);
+		return new HttpResultModel<AccountBillDetailCModel>().setMessage(AccountBillReturnEnum.Success.desc()).setStatus(AccountBillReturnEnum.Success.value())
 				.setResult(model);
 	}
 
