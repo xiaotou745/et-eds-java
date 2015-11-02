e8d213bd8ceb35ee1230b8f72a002165cc29c472
caoheyang <cao.heyang@etaostars.com>
2015-11-02 09:54:05 +0800

    店内任务

---
+++ eds/entity/src/main/java/com/edaisong/entity/domain/InStoreOrderRegionInfo.java
@@ -0,0 +1,10 @@
+package com.edaisong.entity.domain;
+
+/**
+ * 店内任务 区域信息
+ * @author CaoHeYang
+ *
+ */
+public class InStoreOrderRegionInfo {
+
+}
--- eds/entity/src/main/java/com/edaisong/entity/domain/InStoreTask.java
+++ eds/entity/src/main/java/com/edaisong/entity/domain/InStoreTask.java
@@ -1,5 +1,100 @@
 package com.edaisong.entity.domain;
 
 public class InStoreTask {
+	private int businessId;
+	private String businessName;
+	private String businessAddress;
+	private String distanceToBusiness;
+	private InStoreOrderRegionInfo list;
+
+	/**
+	 * 商户id
+	 * 
+	 * @return
+	 */
+	public int getBusinessId() {
+		return businessId;
+	}
+
+	/**
+	 * 商户id
+	 * 
+	 * @param businessId
+	 */
+	public void setBusinessId(int businessId) {
+		this.businessId = businessId;
+	}
+
+	/**
+	 * 商户名称
+	 * 
+	 * @return
+	 */
+	public String getBusinessName() {
+		return businessName;
+	}
+
+	/**
+	 * 商户名称
+	 * 
+	 * @param businessName
+	 */
+	public void setBusinessName(String businessName) {
+		this.businessName = businessName;
+	}
+
+	/**
+	 * 商户地址
+	 * 
+	 * @return
+	 */
+	public String getBusinessAddress() {
+		return businessAddress;
+	}
+
+	/**
+	 * 商户地址
+	 * 
+	 * @param businessAddress
+	 */
+	public void setBusinessAddress(String businessAddress) {
+		this.businessAddress = businessAddress;
+	}
+
+	/**
+	 * 骑士到门店距离
+	 * 
+	 * @return
+	 */
+	public String getDistanceToBusiness() {
+		return distanceToBusiness;
+	}
+
+	/**
+	 * 骑士到门店距离
+	 * 
+	 * @param distanceToBusiness
+	 */
+	public void setDistanceToBusiness(String distanceToBusiness) {
+		this.distanceToBusiness = distanceToBusiness;
+	}
+
+	/**
+	 * 门店下一级 区域 信息
+	 * 
+	 * @return
+	 */
+	public InStoreOrderRegionInfo getList() {
+		return list;
+	}
+
+	/**
+	 * 门店下一级 区域 信息
+	 * 
+	 * @param list
+	 */
+	public void setList(InStoreOrderRegionInfo list) {
+		this.list = list;
+	}
 
 }
