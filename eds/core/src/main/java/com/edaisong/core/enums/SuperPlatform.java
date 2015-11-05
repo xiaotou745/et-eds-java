099b6b86a7d80922e2e3b42286457827fcd2ad51
hulingbo <hu.lingbo.etaostars.com>
2015-11-05 14:03:46 +0800

    系统取消订单

--- eds/core/src/main/java/com/edaisong/core/enums/SuperPlatform.java
+++ eds/core/src/main/java/com/edaisong/core/enums/SuperPlatform.java
@@ -20,7 +20,40 @@ public enum SuperPlatform {
 	/**
 	 * 第三方对接平台
 	 */
-	ThirdParty(4, "第三方对接平台");
+	ThirdParty(4, "第三方对接平台"),
+	/**
+	 * 区域Api发单
+	 */
+	NewApiPush(101, "区域Api发单"),
+	/**
+	 * 区域Api骑士抢单
+	 */
+	NewApiReceive(102, "区域Api骑士抢单"),
+	/**
+	 * 区域Api骑士取货
+	 */
+	NewApiConfirmtake(103, "区域Api骑士取货"),
+	/**
+	 * 区域Api骑士完成订单
+	 */
+	NewApiComplete(104, "区域Api骑士完成订单"),	
+	/**
+	 * 区域后台发单
+	 */
+	NewPlatformPush(201, "区域后台发单"),
+	/**
+	 * 区域后台审核通过
+	 */
+	NewPlatformAuditThrough(202, "区域后台审核通过"),
+	/**
+	 * 区域后台审核拒绝
+	 */
+	NewPlatformAuditRejection(203, "区域后台审核拒绝"),
+	/**
+	 * 区域系统取消订单
+	 */
+	NewSystemCancel(301, "区域系统取消订单");
+	
 	private int value = 0;
 	private String desc;
 	private SuperPlatform(int value, String desc) { // 必须是private的，否则编译错误
