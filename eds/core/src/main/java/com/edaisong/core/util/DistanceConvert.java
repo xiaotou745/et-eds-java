8f249202ab49d37e7d7f903c3260c8153ad571f5
WangChao <wang.chao@etaostars.com>
2015-11-09 17:18:08 +0800

    修改距离

---
+++ eds/core/src/main/java/com/edaisong/core/util/DistanceConvert.java
@@ -0,0 +1,20 @@
+package com.edaisong.core.util;
+
+public class DistanceConvert {
+	
+	public static String ToString(Object o, String defaultValue) {
+		
+		String result = defaultValue;
+		
+		double d = ParseHelper.ToDouble(o, 0);
+		if(0<d && d<1000){
+		 return  ParseHelper.ToString(ParseHelper.ToLong(Math.round(d),0),"")+"米";
+		}
+		if(d>1000){
+			return ParseHelper.ToString(ParseHelper.ToLong(Math.round(d/1000),0),"")+"km";
+		} 
+		 
+		return result;
+	}
+
+}
