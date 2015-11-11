fefac332368b6603a966d520e74296f279761fd9
caoheyang <cao.heyang@etaostars.com>
2015-11-11 10:08:46 +0800

    注释

--- eds/core/src/main/java/com/edaisong/core/util/SmsUtils.java
+++ eds/core/src/main/java/com/edaisong/core/util/SmsUtils.java
@@ -5,21 +5,6 @@ import java.net.MalformedURLException;
 import com.edaisong.core.sms.SmsAPI;
 
 public class SmsUtils {
-	/**
-	 * 发送短信 2015年9月28日 13:12:08 窦海超 Mobile 手机号码，多个号码‘,’号隔开 Content 发送内容
-	 * **/
-	public long testSendSms() {
-		try {
-			return sendSMS("13426401627", "您的验证码为：1234");
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return 0;
-	}
 
 	/**
 	 * 发送短信 2015年9月28日 13:12:08
