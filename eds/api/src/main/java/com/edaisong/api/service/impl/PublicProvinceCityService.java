958ef7a2a8e63ef7f26b3cecb30ee9f1f2707fbe
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-11-16 14:42:14 +0800

    修改和城市相关

--- eds/api/src/main/java/com/edaisong/api/service/impl/PublicProvinceCityService.java
+++ eds/api/src/main/java/com/edaisong/api/service/impl/PublicProvinceCityService.java
@@ -115,7 +115,7 @@ public class PublicProvinceCityService implements IPublicProvinceCityService {
 	public List<AreaModel> getOpenCityListByAccountID(int accountID) {
 		
 		if (accountID == 0) {
-			return getOpenCityByJiBie(2);
+			return getOpenCityByJiBie(3);
 		}else {
 			return publicProvinceCityDao.getOpenCityListByAccountID(accountID);	
 		}
