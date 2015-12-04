1eb7cc529ba523dfed1df061ded5faaf338f1349
douhaichao <dou631@163.com>
2015-12-04 16:15:31 +0800

    quartz服务提交

--- eds/edsservice/src/main/java/com/edaisong/edsservice/service/OrderService.java
+++ eds/edsservice/src/main/java/com/edaisong/edsservice/service/OrderService.java
@@ -3,27 +3,18 @@ package com.edaisong.edsservice.service;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.edaisong.api.service.inter.ITestService;
-import com.edaisong.core.util.SpringBeanHelper;
+import com.edaisong.api.service.impl.TestDouService;
+import com.edaisong.edsservice.Main;
 
 @Service
 public class OrderService implements Job {
-	@Autowired
-	ITestService testService;
-
+	
 	@Override
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
-		ITestService testService = (ITestService) SpringBeanHelper
-				.getCustomBeanByType(ITestService.class);
-
-		// TestService service=new OrderService();
-		testService.insert("1");// new
-								// SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new
-								// Date()) +
+		TestDouService service = Main.contentApp.getBean(TestDouService.class);
+		service.insert("1");
 	}
 
 }
