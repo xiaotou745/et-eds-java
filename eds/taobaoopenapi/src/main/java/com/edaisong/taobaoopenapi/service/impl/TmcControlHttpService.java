e198daece455af27c30156c2c3715d9b2461b4c0
caoheyang <cao.heyang@etaostars.com>
2015-12-04 17:55:46 +0800

    bug

--- eds/taobaoopenapi/src/main/java/com/edaisong/taobaoopenapi/service/impl/TmcControlHttpService.java
+++ eds/taobaoopenapi/src/main/java/com/edaisong/taobaoopenapi/service/impl/TmcControlHttpService.java
@@ -7,12 +7,19 @@ import org.springframework.stereotype.Service;
 import com.edaisong.core.consts.TaoBaoConsts;
 import com.edaisong.core.security.AES;
 import com.edaisong.core.util.HttpUtil;
+import com.edaisong.core.util.ParseHelper;
 import com.edaisong.core.util.PropertyUtils;
+import com.edaisong.entity.taobao.TaoBaoResponseBase;
 import com.edaisong.taobaoopenapi.service.inter.ITmcControlHttpService;
+import com.taobao.api.ApiException;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
 import com.taobao.api.internal.tmc.Message;
 import com.taobao.api.internal.tmc.MessageHandler;
 import com.taobao.api.internal.tmc.MessageStatus;
 import com.taobao.api.internal.tmc.TmcClient;
+import com.taobao.api.request.WaimaiOrderAckRequest;
+import com.taobao.api.response.WaimaiOrderAckResponse;
 import com.taobao.top.link.LinkException;
 
 /**
@@ -54,8 +61,22 @@ public class TmcControlHttpService implements ITmcControlHttpService {
 	 * @date 2015年12月3日 12:05:08
 	 */
 	private void orderDispatch(String data) {
-		String r= HttpUtil.sendPost(PropertyUtils.getProperty("TaoBaoOrderDispatch"), "data="+AES.aesEncrypt(data));
-	    
+		String r = HttpUtil.sendPost(PropertyUtils.getProperty("TaoBaoOrderDispatch"), "data=" + AES.aesEncrypt(data));
+		try {
+			JSONObject jsonObject = new JSONObject(r);
+			if (jsonObject.get("Status").toString() == "1") {
+				TaobaoClient client = new DefaultTaobaoClient(TaoBaoConsts.Uri, TaoBaoConsts.AppKey, TaoBaoConsts.AppSecret);
+				WaimaiOrderAckRequest req = new WaimaiOrderAckRequest();
+				req.setDeliveryOrderNo(ParseHelper.ToLong(jsonObject.get("Result"), 0));
+				WaimaiOrderAckResponse response = client.execute(req, "");
+				TaoBaoResponseBase resp = new TaoBaoResponseBase();
+				resp.setIs_success(response.getAckOrderResult().getSuccess());
+				resp.setError_code(response.getAckOrderResult().getErrorCode());
+				resp.setError_msg(response.getAckOrderResult().getErrorMsg());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -65,8 +86,8 @@ public class TmcControlHttpService implements ITmcControlHttpService {
 	 * @date 20151113
 	 */
 	private void orderClose(String data) {
-	    String r= HttpUtil.sendPost(PropertyUtils.getProperty("TaoBaoCloseOrder"), "data="+AES.aesEncrypt(data));
-	    //TODo 不成功时写日志
+		String r = HttpUtil.sendPost(PropertyUtils.getProperty("TaoBaoCloseOrder"), "data=" + AES.aesEncrypt(data));
+		// TODo 不成功时写日志
 	}
 
 	/**
