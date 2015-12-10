a648cb7e6ef757a034113fd2773c3bfa72c16b49
douhaichao <dou631@163.com>
2015-12-10 18:03:01 +0800

    quartz服务管理

--- eds/api/src/main/java/com/edaisong/api/dao/impl/QuartzServiceDao.java
+++ eds/api/src/main/java/com/edaisong/api/dao/impl/QuartzServiceDao.java
@@ -1,6 +1,8 @@
 package com.edaisong.api.dao.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +17,19 @@ public class QuartzServiceDao extends DaoBase implements IQuartzServiceDao {
 	public List<QuartzServiceModel> query() {
 		return getReadOnlySqlSessionUtil()
 				.selectList("IQuartzServiceDao.query");
+	}
+
+	/**
+	 * @author haichao
+	 * @date 2015年12月10日 10:18:44 修改服务状态
+	 * */
+	@Override
+	public int updateStatus(int id, int status) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		map.put("status", status);
+		return getMasterSqlSessionUtil().update(
+				"IQuartzServiceDao.updateStatus", map);
 	}
 
 }
