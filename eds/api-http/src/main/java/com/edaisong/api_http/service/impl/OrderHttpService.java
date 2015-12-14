de420cfdcf21e23f29892560bbe9af7a8674a499
douhaichao <dou631@163.com>
2015-12-14 18:06:53 +0800

    消息监听

--- eds/api-http/src/main/java/com/edaisong/api_http/service/impl/OrderHttpService.java
+++ eds/api-http/src/main/java/com/edaisong/api_http/service/impl/OrderHttpService.java
@@ -25,7 +25,6 @@ import com.edaisong.entity.common.HttpResultModel;
 import com.edaisong.entity.domain.OrderGrabDetailModel;
 import com.edaisong.entity.domain.InStoreTask;
 import com.edaisong.entity.domain.QueryOrder; 
-import com.edaisong.entity.req.OrderBlancePayReq;
 import com.edaisong.entity.req.OrderChildCancelReq;
 import com.edaisong.entity.req.OrderDetailReq;
 import com.edaisong.entity.req.OrderDraftReq;
@@ -36,7 +35,6 @@ import com.edaisong.entity.req.OrderReq;
 import com.edaisong.entity.req.InStoreTaskReq;
 import com.edaisong.entity.req.OrderStatisticsBReq;
 import com.edaisong.entity.req.QueryOrderReq;  
-import com.edaisong.entity.resp.OrderBlancePayResp;
 import com.edaisong.entity.resp.OrderDetailResp;
 import com.edaisong.entity.resp.OrderDraftResp;
 import com.edaisong.entity.resp.OrderGrabResp;
@@ -136,6 +134,7 @@ public class OrderHttpService implements IOrderHttpService {
 	public HttpResultModel<OrderResp> FlashPush(OrderDraftReq req)
 	{
 
+
 		HttpResultModel<OrderResp> resp=new HttpResultModel<OrderResp>();
 		
 		try
@@ -151,21 +150,6 @@ public class OrderHttpService implements IOrderHttpService {
 		return resp;	
 	}	
 
-	/**
-	 * 余额付款 闪送模式
-	 * @author 胡灵波
-	 * @date 2015年12月14日 11:12:23
-	 * @version 1.0
-	 * @param req
-	 * @return
-	 */
-	@Override
-	public HttpResultModel<OrderBlancePayResp> OrderBalancePay(OrderBlancePayReq req)
-	{
-		HttpResultModel<OrderBlancePayResp> resp=new HttpResultModel<OrderBlancePayResp>();		
-		return orderService.OrderBalancePay(req);
-	}
-	
 
 	/**闪送模式
 	 * 获取订单详情
@@ -182,9 +166,8 @@ public class OrderHttpService implements IOrderHttpService {
 		return orderService.GetOrderDetails(req);
 	}
 
-
 	/**
-	 * 获取小费订单详情 闪送模式
+	 * 获取订单详情 闪送模式
 	 * @author 胡灵波
 	 * @date 2015年12月3日 13:36:24
 	 * @version 1.0
