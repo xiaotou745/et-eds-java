fc3372241eda20bc1091ea0b3d7b57b58ec83b5d
zhaohailong <zhao.hailong@etaostars.com>
2015-12-14 17:13:30 +0800

    添加自动部署工具项目

---
+++ tools/upload2linux/upload2Linux.bat
@@ -0,0 +1,38 @@
+@echo off 
+
+set projectPullPath=D:\eds.java
+set projectPath=%projectPullPath%\eds\admin
+
+set exePath=D:\test
+
+d:
+cd %projectPullPath%
+
+echo 开始pull最新代码
+"C:\Program Files (x86)\Git\cmd\git.exe" pull
+
+cd %projectPath%
+echo 开始打包
+call mvn install
+
+cd %exePath%
+
+echo 开始复制%projectPath%\target\admin-0.0.1-SNAPSHOT.war
+copy /y %projectPath%\target\admin-0.0.1-SNAPSHOT.war war\
+
+
+echo 开始解压
+
+cd %exePath%\unzipwar
+
+jar -xf %exePath%\war\admin-0.0.1-SNAPSHOT.war
+
+echo 开始复制到last目录下
+
+xcopy %exePath%\unzipwar %exePath%\last\ /e /y
+
+cd %exePath%\copytools
+
+echo 开始上传到linux
+java -jar ftptools-0.0.1-SNAPSHOT.jar
+pause
