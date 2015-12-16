477a23900dbc39d414ca3dfe05275b10c6613b64
douhaichao <dou631@163.com>
2015-12-16 16:42:09 +0800

    删除quartz服务

--- eds/edsservice/src/main/bin/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=`dirname $0`
-echo "$DIR"
-cd $DIR
-
-./stop.sh
-sleep 10
-./run.sh
---
+++ eds/mqservice/src/main/java/com/edaisong/mqservice/impl/QuartzJob.java
@@ -0,0 +1,16 @@
+package com.edaisong.mqservice.impl;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class QuartzJob implements Job{
+
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
