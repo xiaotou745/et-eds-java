b152f91e4264dd6ca437996587f05a3e957f81c7
zhaohailong <zhao.hailong@etaostars.com>
2015-12-16 14:43:28 +0800

    调整配置文件

--- tools/upload2linux/upload2Linux.bat
+++ tools/upload2linux/upload2Linux.bat
@@ -1,5 +1,6 @@
 @echo off 
 
+set envconfig=test
 set projectPullPath=D:\eds.java
 set projectName=admin
 set resPath=D:\repository\edaisong\admin\0.0.1-SNAPSHOT
@@ -28,7 +29,10 @@ cd %exePath%\unzipwar
 
 jar -xf %exePath%\war\%projectName%-0.0.1-SNAPSHOT.war
 
-
+echo 开始复制配置文件%envconfig%-conf.properties和%envconfig%-env.properties
+copy /y WEB-INF\classes\conf\custom\%envconfig%-conf.properties WEB-INF\classes\conf\core\current-conf.properties
+copy /y WEB-INF\classes\conf\custom\%envconfig%-env.properties WEB-INF\classes\conf\core\current-env.properties
+pause
 
 cd %exePath%\copytools
 
