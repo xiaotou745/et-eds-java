7159edf137be9383814521776e57cf090348ad88
douhaichao <dou631@163.com>
2016-01-05 14:41:43 +0800

    支付宝平台

--- pays/payapi/src/main/java/com/eds/entity/ParamAliModel.java
+++ pays/payapi/src/main/java/com/eds/entity/ParamAliModel.java
@@ -1,9 +1,55 @@
 package com.eds.entity;
 
 public class ParamAliModel {
+	//取消订单，退款
 	private String trade_no;
 	private Double refund_amount;
 	private String refund_reason;
+	
+	//创建订单 
+	private String notify_url;
+	private String out_trade_no;
+	private String subject;
+	private String total_fee;
+	private String seller_email;
+	private String body;
+	
+	public String getNotify_url() {
+		return notify_url;
+	}
+	public void setNotify_url(String notify_url) {
+		this.notify_url = notify_url;
+	}
+	public String getOut_trade_no() {
+		return out_trade_no;
+	}
+	public void setOut_trade_no(String out_trade_no) {
+		this.out_trade_no = out_trade_no;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getTotal_fee() {
+		return total_fee;
+	}
+	public void setTotal_fee(String total_fee) {
+		this.total_fee = total_fee;
+	}
+	public String getSeller_email() {
+		return seller_email;
+	}
+	public void setSeller_email(String seller_email) {
+		this.seller_email = seller_email;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
 	public String getTrade_no() {
 		return trade_no;
 	}
